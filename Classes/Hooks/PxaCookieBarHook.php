40f7c7334906c2777031cb9911faac2633d9da10
Andriy Oprysko <andriy@pixelant.se>
2016-05-10 14:03:10 +0300

    added checking if cookie bar was included in template to disable cookie

--- Classes/Hooks/PxaCookieBarHook.php
+++ Classes/Hooks/PxaCookieBarHook.php
@@ -10,7 +10,9 @@ class PxaCookieBarHook {
      * @param $reference
      */
 	public function contentPostProc_all(&$params, $reference){
-		if(!isset($_COOKIE['pxa_cookie_warning']) &&  (intval($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_pxacookiebar.']['settings.']['disableCookies']) == 1)) {
+		if(!isset($_COOKIE['pxa_cookie_warning'])
+            &&  (intval($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_pxacookiebar.']['settings.']['disableCookies']) == 1)
+            && (intval($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_pxacookiebar.']['settings.']['enable']) == 1)) {
 			if(session_id()) {	
 				setcookie(session_name(), session_id(), time()-60*60*24, '/');        
 		        session_unset();
