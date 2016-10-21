316a1105901fca02d0cf48d936ee631253e4689a
Pavlo Zaporozkyi <pavlo@pixelant.se>
2016-10-21 12:35:18 +0300

    [TASK] Update TYPO3 version dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'extbase' => '1.3',
 			'fluid' => '1.3',
-			'typo3' => '6.0.0-6.2.99',
+			'typo3' => '6.0.0-7.6.99',
 		),
 		'conflicts' => array(
 		),
